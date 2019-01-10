71cb5eafa1ebd336f42707215790077e93446cfc
alexharb <alex.harb11@gmail.com>
2019-01-09 20:30:02 -0800

    "Created two new dancers and added a resource folder for images"

---
+++ src/bounceDancer.js
@@ -0,0 +1,25 @@
+var makeBounceDancer = function(top, left, timeBetweenSteps) {
+  makeDancer.call(this, top, left, timeBetweenSteps);
+  //this.time = timeBetweenSteps;
+  this.$node.attr('id', 'bounceDancer');
+  this.$node.append('<img src="res/dancingQueen.png" />');
+};
+
+makeBounceDancer.prototype = Object.create(makeDancer.prototype);
+
+makeBounceDancer.prototype.constructor = makeBounceDancer;
+
+makeBounceDancer.prototype.step = function() {
+  // toggle() is a jQuery method to show/hide the <span> tag.
+  // See http://api.jquery.com/category/effects/ for this and
+  // other effects you can use on a jQuery-wrapped html tag.
+  /* var rotation = function() {
+    this.$node.rotate({
+      angle:0,
+      animateTo:360,
+      callback: rotation
+    });
+  }; 
+  rotation.call(this); */
+  makeDancer.prototype.step.call(this);
+};
---
+++ src/spinDancer.js
@@ -0,0 +1,34 @@
+var makeSpinDancer = function(top, left, timeBetweenSteps) {
+  makeDancer.call(this, top, left, limitTime(timeBetweenSteps));
+  //this.time = timeBetweenSteps;
+  this.$node.attr('id', 'spinDancer');
+  this.$node.append('<img src="res/dancingQueen.png" />');
+};
+
+makeSpinDancer.prototype = Object.create(makeDancer.prototype);
+
+makeSpinDancer.prototype.constructor = makeSpinDancer;
+
+makeSpinDancer.prototype.step = function() {
+  // toggle() is a jQuery method to show/hide the <span> tag.
+  // See http://api.jquery.com/category/effects/ for this and
+  // other effects you can use on a jQuery-wrapped html tag.
+  var rotation = function() {
+    this.$node.rotate({
+      angle:0,
+      animateTo:360,
+      callback: rotation
+    });
+  };
+  rotation.call(this);
+  makeDancer.prototype.step.call(this);
+};
+
+
+var limitTime = function limitTime(time) {
+  if (time < 500) {
+    return 500;
+  } else {
+    return time;
+  }
+}
