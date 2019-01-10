c8756104cb6d7a6410b48cbc443116e2aecb526b
alexharb <alex.harb11@gmail.com>
2019-01-09 21:10:10 -0800

    "Added bounce animation for bounceDancer"

--- src/bounceDancer.js
+++ src/bounceDancer.js
@@ -1,6 +1,6 @@
 var makeBounceDancer = function(top, left, timeBetweenSteps) {
-  makeDancer.call(this, top, left, timeBetweenSteps);
-  //this.time = timeBetweenSteps;
+  makeDancer.call(this, top, left, limitTime(timeBetweenSteps));
+  this.time = timeBetweenSteps;
   this.$node.attr('id', 'bounceDancer');
   this.$node.append('<img src="res/dancingQueen.png" />');
 };
@@ -13,5 +13,12 @@ makeBounceDancer.prototype.step = function() {
   // toggle() is a jQuery method to show/hide the <span> tag.
   // See http://api.jquery.com/category/effects/ for this and
   // other effects you can use on a jQuery-wrapped html tag.
+
+  this.$node.addClass('animated bounce infinite');
   makeDancer.prototype.step.call(this);
+};
+
+
+var limitTime = function limitTime(time) {
+  return time + 1000;
 };
